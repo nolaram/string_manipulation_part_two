c60cc08b2137c5edc53791da84fe4ece875c57b9
nolaram <marloncopino10@gmail.com>
2025-03-26 18:29:53 +0800

    swap_casing

--- practice_programs_batch_six/swapcase_functionality_part_two.py
+++ practice_programs_batch_six/swapcase_functionality_part_two.py
@@ -2,4 +2,14 @@
 user_name = str(input("Enter your name: "))
 
 # convert the casing
+swap_case = ""
+
+for char in user_name:
+    if "a" <= char <= "z":
+        swap_case += chr(ord(char) - 32)
+    elif "A" <= char <= "Z":
+        swap_case += chr(ord(char) + 32)
+    else:
+        swap_case += char
+
 # print the output
