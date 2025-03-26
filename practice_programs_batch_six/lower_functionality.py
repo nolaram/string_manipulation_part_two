0d5055508c22adb3db433e34a74e08ed404dc525
nolaram <marloncopino10@gmail.com>
2025-03-26 16:48:07 +0800

    convert_characters_to_lower_case

--- practice_programs_batch_six/lower_functionality.py
+++ practice_programs_batch_six/lower_functionality.py
@@ -2,4 +2,12 @@
 user_name = str(input("Enter your name: "))
 
 # convert the input to lower case characters
+lower_case = ""
+
+for char in user_name:
+    if "A" <= char <= "Z":
+        lower_case += chr(ord(char) + 32)
+    else:
+        lower_case += char
+
 # print the output
