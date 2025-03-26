5cbcb5eaf1c275a428153af2b999d7a7597ea27e
nolaram <marloncopino10@gmail.com>
2025-03-26 17:19:26 +0800

    check_if_ending_with_lon

--- practice_programs_batch_six/endswith_functionality.py
+++ practice_programs_batch_six/endswith_functionality.py
@@ -2,6 +2,10 @@
 user_name = str(input("Enter your name: "))
 
 # check if his/her name ends with "lon"
+ends_with = "lon"
+
+position = user_name.find(ends_with)
+
 # if name ends with lon
     # print True
 # if not
