0f248ebbeb56044f137b118fea4411138defab34
nolaram <marloncopino10@gmail.com>
2025-03-26 18:07:43 +0800

    seperate_white_space

--- practice_programs_batch_six/center_functionality.py
+++ practice_programs_batch_six/center_functionality.py
@@ -5,5 +5,9 @@ user_name = str(input("Enter your name: "))
 space = 10
 
 # seperate the space
+white_space = max(0, space - len(user_name))
+left_space = white_space // 2
+right_space = white_space - left_space
+
 # add the space 
 # print output
