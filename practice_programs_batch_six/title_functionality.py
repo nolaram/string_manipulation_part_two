7e57570c58f60b40f58d2f7eed7bb3269d30f678
nolaram <marloncopino10@gmail.com>
2025-03-26 18:52:01 +0800

    changed_since_the_output_is_not_working

--- practice_programs_batch_six/title_functionality.py
+++ practice_programs_batch_six/title_functionality.py
@@ -1,14 +1,5 @@
 # ask user for his/her name but with wrong inputs
 user_name = str(input("Enter your name: "))
 
-# split the input into words
-user_name = user_name.split()
-
-# capitalize the words
-swap_case = [word[0].upper() + word[1:].lower() for word in user_name]
-
-# add space
-add_space = " ".join(swap_case)
-
-# print output
-print(add_space)
+# convert casing
+words = [user_name[0]]
