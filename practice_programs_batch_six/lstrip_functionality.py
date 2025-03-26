ae47685282cdd31a4e8880c84523d75af6010671
nolaram <marloncopino10@gmail.com>
2025-03-26 15:57:29 +0800

    user_input_with_spaces

--- practice_programs_batch_six/lstrip_functionality.py
+++ practice_programs_batch_six/lstrip_functionality.py
@@ -1,3 +1,5 @@
 # ask user to input with spaces in the beginning
+user_input = str(input("Enter a character with spaces in the beginning: "))
+
 # remove the spaces in the beginning without using lstrip
 # print the output
