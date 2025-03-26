9951c9e6bb61520f563e414f715ef865536388b7
nolaram <marloncopino10@gmail.com>
2025-03-26 16:30:17 +0800

    remove_prefix_without_removeprefix()

--- practice_programs_batch_six/remove_prefix_functionality.py
+++ practice_programs_batch_six/remove_prefix_functionality.py
@@ -2,6 +2,9 @@
 user_name = str(input("Enter your name: "))
 
 # remove the prefix "Mar" if it's in the input
+if user_name.startswith("Mar"):
+    user_name = user_name.replace("Mar", "")
+    
     # print the output
 # if not
     # print the output
