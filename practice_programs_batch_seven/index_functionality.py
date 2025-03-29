1592e32364bfa0bf8d463b74dc144aaf4f232c26
nolaram <marloncopino10@gmail.com>
2025-03-29 18:00:13 +0800

    user_name_input

--- practice_programs_batch_seven/index_functionality.py
+++ practice_programs_batch_seven/index_functionality.py
@@ -1,4 +1,6 @@
 # ask user for his/her name
+user_name = str(input("Enter your name: "))
+
 # set the variable that you want to find in the input
 # set the position to -1 incase the letter is not found
 # look for the position where the variable first appears
