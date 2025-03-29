688c4eba2e30acac8aeeb2f16af09fb386e593e1
nolaram <marloncopino10@gmail.com>
2025-03-29 17:44:39 +0800

    check_how_many_times_the_variable_appears

--- practice_programs_batch_seven/count_functionality.py
+++ practice_programs_batch_seven/count_functionality.py
@@ -6,6 +6,8 @@ letter = "a"
 
 # check how many times the variable appears
 # set count to 0
+count = 0
+
 # for loop to check every character in input
         # increase count by 1 every time the character matches the variable
 # print the output
