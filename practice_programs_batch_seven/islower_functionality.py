561aa44b72c50f59107336774fb1e92b5512671a
nolaram <marloncopino10@gmail.com>
2025-03-29 16:13:51 +0800

    pseudocode_for_program_four

---
+++ practice_programs_batch_seven/islower_functionality.py
@@ -0,0 +1,10 @@
+# ask user for his/her name
+# make a variable that has a value of true
+# for loop to check every character
+    # if one character is upper case
+        # change the variable into false
+        # stop the program
+# if all lower case
+    # print true
+# if not
+    # print false
