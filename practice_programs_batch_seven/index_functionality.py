fba738e3ba7f01d9c26131b66c4d6dd14c3978fa
nolaram <marloncopino10@gmail.com>
2025-03-29 18:01:19 +0800

    set_position_to_negative_one

--- practice_programs_batch_seven/index_functionality.py
+++ practice_programs_batch_seven/index_functionality.py
@@ -5,6 +5,8 @@ user_name = str(input("Enter your name: "))
 letter = "a"
 
 # set the position to -1 incase the letter is not found
+position = -1
+
 # look for the position where the variable first appears
 # if the letter is found
     # print the position
