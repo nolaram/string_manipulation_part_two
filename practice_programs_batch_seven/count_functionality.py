a6a99f56ada0172958393d1c39cd32d2cf89c520
nolaram <marloncopino10@gmail.com>
2025-03-29 17:43:01 +0800

    set_variable

--- practice_programs_batch_seven/count_functionality.py
+++ practice_programs_batch_seven/count_functionality.py
@@ -2,6 +2,8 @@
 user_name = str(input("Enter your name: "))
 
 # set the variable with a value "a"
+letter = "a"
+
 # check how many times the variable appears
 # set count to 0
 # for loop to check every character in input
