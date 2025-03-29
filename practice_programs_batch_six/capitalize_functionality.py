2ca1a8f6e064f41f0862e931c104d4fc5fcaa9b3
nolaram <marloncopino10@gmail.com>
2025-03-29 13:53:37 +0800

    capitalize_first_letter

--- practice_programs_batch_six/capitalize_functionality.py
+++ practice_programs_batch_six/capitalize_functionality.py
@@ -2,4 +2,12 @@
 user_name = str(input("Enter your name: "))
 
 # capitalize the casing
+capitalize = []
+
+
+if user_name:
+    output = user_name[0].upper() + user_name[1:].lower()
+else:
+    output = ""
+
 # print the output
