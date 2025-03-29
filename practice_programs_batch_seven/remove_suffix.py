8e9c257078d8fa28e023787cbd4879c73991f251
nolaram <marloncopino10@gmail.com>
2025-03-29 15:46:40 +0800

    pseudocode_for_program_two

---
+++ practice_programs_batch_seven/remove_suffix.py
@@ -0,0 +1,7 @@
+# ask user for his/her name
+# check if matches with the same suffix "lon"
+# if yes
+    # replace the suffix with blank
+        # print the name
+# if no
+    # print the input without changes
