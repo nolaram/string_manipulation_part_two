56a0a88843b952a7ec28fca7143f3b25d8d336f8
nolaram <marloncopino10@gmail.com>
2025-03-29 17:22:12 +0800

    replace_space_with_zero

--- practice_programs_batch_seven/zfill_functionality.py
+++ practice_programs_batch_seven/zfill_functionality.py
@@ -6,6 +6,8 @@ value = 6
 
 # fill the three numbers with zero in the beginning so it becomes six digits
 # add space in the beginning depending on the value
-space = " " * value - len(user_name) + user_name
+space = " " * (value - len(user_name)) + user_name
 # replace the spaces with zero
+zfill = space.replace(" ", "0")
+
 # print the output
