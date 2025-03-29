6049f2e32046bca7ffa98fe3b15132b5292db356
nolaram <marloncopino10@gmail.com>
2025-03-29 17:40:01 +0800

    pseudocode_for_program_eight

---
+++ practice_programs_batch_seven/count_functionality.py
@@ -0,0 +1,7 @@
+# ask user for his/her name
+# set the variable with a value "a"
+# check how many times the variable appears
+# set count to 0
+# for loop to check every character in input
+        # increase count by 1 every time the character matches the variable
+# print the output
