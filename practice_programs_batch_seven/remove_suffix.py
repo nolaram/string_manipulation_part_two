0229f79eabea3442e560dc5c215501730a2362bc
nolaram <marloncopino10@gmail.com>
2025-03-29 15:50:05 +0800

    if_same_suffix_replace_with_blank

--- practice_programs_batch_seven/remove_suffix.py
+++ practice_programs_batch_seven/remove_suffix.py
@@ -3,7 +3,9 @@ user_name = str(input("Enter your name: "))
 
 # check if matches with the same suffix "lon"
 # if yes
+if user_name.endswith("lon"):
     # replace the suffix with blank
+    user_name = user_name.replace("lon", "")
         # print the name
 # if no
     # print the input without changes
