2f84987f9e9aaec4a5837cf73171f9331089a9b8
nolaram <marloncopino10@gmail.com>
2025-03-29 18:00:45 +0800

    set_letter_you_want_to_find

--- practice_programs_batch_seven/index_functionality.py
+++ practice_programs_batch_seven/index_functionality.py
@@ -2,6 +2,8 @@
 user_name = str(input("Enter your name: "))
 
 # set the variable that you want to find in the input
+letter = "a"
+
 # set the position to -1 incase the letter is not found
 # look for the position where the variable first appears
 # if the letter is found
