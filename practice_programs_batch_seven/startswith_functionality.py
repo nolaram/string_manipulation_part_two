dd2b961040507882d37acd10f85c765c8ef2bed6
nolaram <marloncopino10@gmail.com>
2025-03-29 16:45:08 +0800

    user_name_input

--- practice_programs_batch_seven/startswith_functionality.py
+++ practice_programs_batch_seven/startswith_functionality.py
@@ -1,4 +1,6 @@
 # ask user for his/her name
+user_name = str(input("Enter your name: "))
+
 # set a variable with the prefix that you want to find
 # find the prefix in the input
 # if the input startswith the variable given
