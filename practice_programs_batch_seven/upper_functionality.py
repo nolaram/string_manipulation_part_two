5b14b0a16851c3b8f9d54a2a9412f2af3d3b3b30
nolaram <marloncopino10@gmail.com>
2025-03-29 15:57:01 +0800

    pseudocode_for_program_three

---
+++ practice_programs_batch_seven/upper_functionality.py
@@ -0,0 +1,8 @@
+# ask user for his/her name
+# make a blank variable
+# for loop to check every character in the input
+    # if the character is lower case
+        # change it to upper case
+    # if already upper case
+        # no changes
+# print the output
