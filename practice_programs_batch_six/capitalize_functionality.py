804f80241671d4bd29a9e6f1331e691131cf7b70
nolaram <marloncopino10@gmail.com>
2025-03-29 13:44:02 +0800

    pseudocode_for_program_nine

--- practice_programs_batch_six/capitalize_functionality.py
+++ practice_programs_batch_six/capitalize_functionality.py
@@ -1,16 +1,3 @@
 # ask user for his/her name
-user_name = str(input("Enter your name: "))
-
-# convert the casing
-swap_case = ""
-
-for char in user_name:
-    if "a" <= char <= "z":
-        swap_case += chr(ord(char) - 32)
-    elif "A" <= char <= "Z":
-        swap_case += chr(ord(char) + 32)
-    else:
-        swap_case += char
-
+# capitalize the casing
 # print the output
-print(swap_case)
