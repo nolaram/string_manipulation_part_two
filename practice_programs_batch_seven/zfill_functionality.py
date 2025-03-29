2aff8ff6ff8c5601f0ef99ba0a42d3d6c8fadcdb
nolaram <marloncopino10@gmail.com>
2025-03-29 17:20:31 +0800

    add_space

--- practice_programs_batch_seven/zfill_functionality.py
+++ practice_programs_batch_seven/zfill_functionality.py
@@ -6,5 +6,6 @@ value = 6
 
 # fill the three numbers with zero in the beginning so it becomes six digits
 # add space in the beginning depending on the value
+space = " " * value - len(user_name) + user_name
 # replace the spaces with zero
 # print the output
