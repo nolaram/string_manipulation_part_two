7858cec3940015b0509e6e08f99f12382cd63619
nolaram <marloncopino10@gmail.com>
2025-03-29 16:44:20 +0800

    pseudocode_for_program_five

---
+++ practice_programs_batch_seven/startswith_functionality.py
@@ -0,0 +1,7 @@
+# ask user for his/her name
+# set a variable with the prefix that you want to find
+# find the prefix in the input
+# if the input startswith the variable given
+    # print true
+# if not
+    # print false
