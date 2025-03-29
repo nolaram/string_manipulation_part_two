5cd010470775397cb1e4f07414d96fb0d9ca39d0
nolaram <marloncopino10@gmail.com>
2025-03-29 13:23:04 +0800

    make_list

--- practice_programs_batch_six/title_functionality.py
+++ practice_programs_batch_six/title_functionality.py
@@ -1,6 +1,9 @@
 # ask user for input
 user_name = str(input("Enter your name: "))
+sentence = user_name.split()
 
 # list to make things easier
+upper = []
+
 # convert first letter to uppercase and the rest to lower letters
 # print the output
