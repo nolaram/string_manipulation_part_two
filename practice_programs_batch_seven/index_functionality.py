552365a0506c36a1006b189895ef333dd4602ed2
nolaram <marloncopino10@gmail.com>
2025-03-29 17:59:31 +0800

    pseudocode_for_program_nine

---
+++ practice_programs_batch_seven/index_functionality.py
@@ -0,0 +1,8 @@
+# ask user for his/her name
+# set the variable that you want to find in the input
+# set the position to -1 incase the letter is not found
+# look for the position where the variable first appears
+# if the letter is found
+    # print the position
+# if not
+    # print none
