5a3852aa5b31815275800362173437d52594fcc5
nolaram <marloncopino10@gmail.com>
2025-03-29 17:18:41 +0800

    value_of_zero

--- practice_programs_batch_seven/zfill_functionality.py
+++ practice_programs_batch_seven/zfill_functionality.py
@@ -1,7 +1,9 @@
 # ask three numbers from the user
 user_name = str(input("Enter three numbers: "))
 
-# value of zero you want to add 
+# value of zero you want to add
+value = 6
+
 # fill the three numbers with zero in the beginning so it becomes six digits
 # add space in the beginning depending on the value
 # replace the spaces with zero
