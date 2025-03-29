d3f3173bb0c92c006db555194818402e41cb1efa
nolaram <marloncopino10@gmail.com>
2025-03-29 13:21:17 +0800

    reset_code_due_to_errors

--- practice_programs_batch_six/title_functionality.py
+++ practice_programs_batch_six/title_functionality.py
@@ -1,17 +1,6 @@
-# ask user for his/her name but with wrong inputs
+# ask user for input
 user_name = str(input("Enter your name: "))
 
-# convert casing
-words = [user_name[0]]
-
-for char in user_name [1:]:
-    if char.isupper():
-        words.append(" " + char)
-    else:
-        words[-1] += char
-
-# add space and capitalize
-add_space = " ".join(word.capitalize() for word in words)
-
+# list to make things easier
+# convert first letter to uppercase and the rest to lower letters
 # print the output
-print(add_space)
