5601dd85e99f2864efa200963c5249a47259d0b9
nolaram <marloncopino10@gmail.com>
2025-03-29 16:14:53 +0800

    user_name_input

--- practice_programs_batch_seven/islower_functionality.py
+++ practice_programs_batch_seven/islower_functionality.py
@@ -1,4 +1,6 @@
 # ask user for his/her name
+user_name = str(input("Enter your name: "))
+
 # make a variable that has a value of true
 # for loop to check every character
     # if one character is upper case
