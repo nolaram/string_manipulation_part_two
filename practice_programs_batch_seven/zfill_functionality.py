1221d86d42dc54cf1a290a8569ee028ec4a50bbd
nolaram <marloncopino10@gmail.com>
2025-03-29 17:14:12 +0800

    pseudocode_for_program_seven

---
+++ practice_programs_batch_seven/zfill_functionality.py
@@ -0,0 +1,6 @@
+# ask three numbers from the user
+# value of zero you want to add 
+# fill the three numbers with zero in the beginning so it becomes six digits
+# add space in the beginning depending on the value
+# replace the spaces with zero
+# print the output
