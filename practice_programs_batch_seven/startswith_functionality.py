50394a7b4b95934c4d9734e18a11437675a65c60
nolaram <marloncopino10@gmail.com>
2025-03-29 16:45:52 +0800

    variable_you_want_to_find

--- practice_programs_batch_seven/startswith_functionality.py
+++ practice_programs_batch_seven/startswith_functionality.py
@@ -2,6 +2,8 @@
 user_name = str(input("Enter your name: "))
 
 # set a variable with the prefix that you want to find
+starts_with = "Mar"
+
 # find the prefix in the input
 # if the input startswith the variable given
     # print true
